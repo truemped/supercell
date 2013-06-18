24b1191e25467154af3ad39c19991d8e8c86f49c
Daniel Truemper <truemper@retresco.de>
2013-06-18 22:01:07 +0200

    Fixing sort comparator issue on Py 3.3

--- supercell/utils/acceptparsing.py
+++ supercell/utils/acceptparsing.py
@@ -71,5 +71,5 @@ def parse_accept_header(accept):
             else:
                 media_params.append((key, value))
         result.append((media_type, dict(media_params), q))
-    result.sort(lambda x, y: -cmp(x[2], y[2]))
+    result.sort(cmp=lambda x, y: -cmp(x[2], y[2]))
     return result
