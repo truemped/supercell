d9c35dff86a70b7b49b4f737b081edd1ff799772
Daniel Truemper <truemper@retresco.de>
2013-07-01 12:16:42 +0200

    Base consumer and consumer mapping

---
+++ supercell/api/consumer.py
@@ -0,0 +1,116 @@
+# vim: set fileencoding=utf-8 :
+#
+# Copyright (c) 2013 Daniel Truemper <truemped at googlemail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+from __future__ import absolute_import, division, print_function, with_statement
+
+from collections import defaultdict
+import json
+
+from supercell._compat import with_metaclass
+from supercell.api import ContentType
+from supercell.utils import parse_accept_header
+
+
+__all__ = ['NoConsumerFound', 'ConsumerBase', 'JsonConsumer']
+
+
+class NoConsumerFound(Exception):
+    '''Raised if no matching consumer for the client's `Content-Type` header was
+    found.'''
+    pass
+
+
+class ConsumerMeta(type):
+    '''Meta class for all content type providers.
+
+    This will simply register a consumer with the respective content type
+    information and make them available in a list of content types and their
+    consumer.
+    '''
+
+    KNOWN_CONTENT_TYPES = defaultdict(list)
+
+    def __new__(cls, name, bases, dct):
+        consumer_class = type.__new__(cls, name, bases, dct)
+
+        if name != 'ConsumerBase' and hasattr(consumer_class, 'CONTENT_TYPE'):
+            ct = consumer_class.CONTENT_TYPE
+            ConsumerMeta.KNOWN_CONTENT_TYPES[ct.content_type].append(
+                    (ct, consumer_class))
+
+        return consumer_class
+
+
+class ConsumerBase(with_metaclass(ConsumerMeta, object)):
+    '''Base class for content type providers.'''
+
+    CONTENT_TYPE = None
+    '''The target content type for the provider.'''
+
+    @staticmethod
+    def map_consumer(content_type, handler):
+        '''Map a given content type to the correct provider implementation.
+
+        If no provider matches, raise a `NoProviderFound` exception.
+
+        :param accept_header: HTTP Accept header value
+        :type accept_header: str
+        :param handler: supercell request handler
+        '''
+        accept = parse_accept_header(content_type)
+        if len(accept) == 0:
+            raise NoConsumerFound()
+
+        (ctype, params, q) = accept[0]
+
+        if ctype not in handler._CONS_CONTENT_TYPES:
+            raise NoConsumerFound()
+
+        c = ContentType(ctype, vendor=params.get('vendor', None),
+                        version=params.get('version', None))
+        if c not in handler._CONS_CONTENT_TYPES[ctype]:
+            raise NoConsumerFound()
+
+        known_types = [t for t in ConsumerMeta.KNOWN_CONTENT_TYPES[ctype]
+                       if t[0] == c]
+
+        if len(known_types) == 1:
+            return known_types[0][1]
+
+        raise NoConsumerFound()
+
+    def consume(self, handler):
+        '''This method should return the correct representation as a simple
+        string (i.e. byte buffer) that will be used as return value.
+
+        :param model: the model to convert to a certain content type
+        :type model: supercell.schematics.Model
+        '''
+        raise NotImplemented()
+
+
+class JsonConsumer(ConsumerBase):
+    '''Default `application/json` provider.'''
+
+    CONTENT_TYPE = ContentType('application/json', None, None)
+
+    def consume(self, handler):
+        '''Simply return the json via `json.dumps`.
+
+        .. seealso:: :py:mod:`supercell.api.provider.ProviderBase.provide`
+        '''
+        pass
---
+++ test/test_consumer.py
@@ -0,0 +1,83 @@
+# vim: set fileencoding=utf-8 :
+#
+# Copyright (c) 2013 Daniel Truemper <truemped at googlemail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+from __future__ import absolute_import, division, print_function, with_statement
+
+import sys
+if sys.version_info > (2, 7):
+    from unittest import TestCase
+else:
+    from unittest2 import TestCase
+
+from supercell.api import consumes, RequestHandler
+from supercell.api.metatypes import ContentType
+from supercell.api.consumer import ConsumerBase, JsonConsumer, NoConsumerFound
+
+
+class MoreDetailedJsonConsumer(JsonConsumer):
+
+    CONTENT_TYPE = ContentType('application/json', vendor='supercell')
+
+
+class JsonConsumerWithVendorAndVersion(JsonConsumer):
+
+    CONTENT_TYPE = ContentType('application/json', vendor='supercell',
+                               version=1.0)
+
+
+class TestBasicConsumer(TestCase):
+
+    def test_default_json_consumer(self):
+
+        @consumes('application/json')
+        class MyHandler(RequestHandler):
+            pass
+
+        consumer = ConsumerBase.map_consumer('application/json',
+                                             handler=MyHandler)
+
+        self.assertIs(consumer, JsonConsumer)
+
+        with self.assertRaises(NoConsumerFound):
+            ConsumerBase.map_consumer('application/vnd.supercell-v1.1+json',
+                                      handler=MyHandler)
+
+    def test_specific_json_consumer(self):
+
+        @consumes('application/json', vendor='supercell')
+        class MyHandler(RequestHandler):
+            pass
+
+        consumer = ConsumerBase.map_consumer('application/vnd.supercell+json',
+                                             handler=MyHandler)
+        self.assertIs(consumer, MoreDetailedJsonConsumer)
+
+        with self.assertRaises(NoConsumerFound):
+            ConsumerBase.map_consumer('application/json', handler=MyHandler)
+
+    def test_json_consumer_with_version(self):
+
+        @consumes('application/json', vendor='supercell', version=1.0)
+        class MyHandler(RequestHandler):
+            pass
+
+        consumer = ConsumerBase.map_consumer(
+                'application/vnd.supercell-v1.0+json', handler=MyHandler)
+        self.assertIs(consumer, JsonConsumerWithVendorAndVersion)
+
+        with self.assertRaises(NoConsumerFound):
+            ConsumerBase.map_consumer('application/json', handler=MyHandler)
